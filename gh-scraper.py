396697ec1308c3a9e9d922bb02df9cb61c24ef48
Daniel <satelerd@gmail.com>
2021-10-13 19:00:33 -0300

    Data visualisation

--- gh-scraper.py
+++ gh-scraper.py
@@ -43,13 +43,38 @@ def generate_plot(dates, data):
     Funcion que dado un diccionario de contribuciones, genera un grafico con ellas.
     """
 
-    plt.figure(figsize=(10, 5))
-    plt.scatter(dates, data)
+    # plt.figure(figsize=(10, 5))
+    # plt.scatter(dates, data)
+    # plt.show()
+
+    # Se crea una matriz de ceros de tamaño len(data_level)/7 x 7
+    data_matrix = np.zeros((int(len(data_level) / 7), 7))
+    # Se llena la matriz con los datos de data_count
+    for i in range(len(data_level)):
+        try:
+            data_matrix[int(i / 7), i % 7] = data_level[i]
+
+        except:  # NO SE LLENA LA MATRIZ COMPLETA, OJO!!!!!
+            break
+
+    # Se crea una figura y una subfigura
+    fig, ax = plt.subplots()
+    # Se grafica la matriz
+    ax.imshow(data_matrix)
+    # Se agrega el titulo
+    ax.set_title("Contribuciones")
+    # Se agrega el label a cada eje
+    ax.set_xlabel("Dias")
+    ax.set_ylabel("Semana")
+    # Se agrega el label a cada celda
+    for i in range(len(data)):
+        ax.text(i % 7, int(i / 7), data[i], ha="center", va="center", color="w")
+    # Se muestra el grafico
     plt.show()
 
 
 dates, data_count, data_level = get_user_contributions("satelerd")
-print(dates)
-print(data_count)
-print(data_level)
+# print(dates)
+# print(data_count)
+# print(data_level)
 generate_plot(dates, data_level)
