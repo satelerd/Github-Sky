3c6290a2a5ff86a5540ad3b961f1fc14f6abf42a
Daniel <satelerd@gmail.com>
2021-10-13 20:20:05 -0300

    Initial work on star_generator()

--- github-sky-generator.py
+++ github-sky-generator.py
@@ -38,14 +38,50 @@ def get_user_contributions(user):
 
 
 # GRAPH GENERATOR
+def star_generator(data):
+    # Se crea una matriz de ceros de tamaño len(data_level)/7 x 7
+    data_matrix = np.zeros((int(len(data) / 7), 7))
+    data_matrix[0, 0] = 5
+    data_matrix[2, 2] = 5
+    data_matrix[2, 5] = 5
+    data_matrix = data_matrix.T
+    print(data_matrix)
+
+    # Se llena la matriz con los datos de data_count
+    for i in range(len(data)):
+        try:
+            data_matrix[int(i / 7), i % 7] = data[i]
+
+        except:  # NO SE LLENA LA MATRIZ COMPLETA, OJO!!!!!
+            break
+
+    
+    # Generamos la matriz principal que sera graficada   (matriz y desp pasarlo a lista para graficarlo mejor... en volaa)
+    weeks = 52
+    days = 7
+    rows_day, cols_day = 100
+    for week in range(weeks):
+
+        for day in range(days):
+
+            # aca definimos el valor de cuantas estrellas generar en ese dia
+
+            for row in range(rows_day):
+                for col in range(cols_day):
+
+                    # aqui vamos agregando un valor binario si hay o no estrella
+
+
+
+            # aqui se deberia agregar los datos de cada dia a la matriz principal
+
+    # una vez generado todo, hay que graficarlo
+
+
 def generate_plot(dates, data):
     """
     Funcion que dado un diccionario de contribuciones, genera un grafico con ellas.
     """
-
-    # plt.figure(figsize=(10, 5))
-    # plt.scatter(dates, data)
-    # plt.show()
 
     # Se crea una matriz de ceros de tamaño len(data_level)/7 x 7
     data_matrix = np.zeros((int(len(data_level) / 7), 7))
@@ -61,18 +97,15 @@ def generate_plot(dates, data):
     fig, ax = plt.subplots()
     # Se grafica la matriz
     ax.imshow(data_matrix)
-    # Se agrega el titulo
     ax.set_title("Contribuciones")
-    # Se agrega el label a cada eje
     ax.set_xlabel("Dias")
     ax.set_ylabel("Semana")
     # Se agrega el label a cada celda
     for i in range(len(data)):
         ax.text(i % 7, int(i / 7), data[i], ha="center", va="center", color="w")
-    # Se muestra el grafico
     plt.show()
 
 
 # FUNCTION CALL
 dates, data_count, data_level = get_user_contributions("satelerd")
-generate_plot(dates, data_level)
+star_generator(data_level)
