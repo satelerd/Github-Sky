d0d4c9ecdd7905c04f8bb25e0afea912583b0502
Daniel <satelerd@gmail.com>
2021-10-14 22:55:41 -0300

    cleaning and translating

--- github-sky-generator.py
+++ github-sky-generator.py
@@ -1,7 +1,3 @@
-"""
-Lo siguiente es el codigo para un proyecto que se llama Github Sky, en el cual dado un usuario de github, hace un scrape con bs4 para sacar los datos de contribucion (o usar una api si existe) y desp generar una foto del cielo con estrellas representando el patron de contribuciones del usuario. 
-"""
-
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
@@ -11,14 +7,14 @@ import matplotlib.pyplot as plt
 # SCRAPER
 def get_user_contributions(user):
     """
-    Funcion que dado un usuario de github, devuelve la cantidad de contribuciones que ha hecho.
+    Function that given a github profile, returns the cuantity of contributions that the user has made
     """
 
     url = f"https://github.com/{user}"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
 
-    # Busca todas los tags llamadss 'rect'
+    # Search for all tags call 'rect'
     contributions = soup.find_all("rect")
     # print(contributions)
 
@@ -26,11 +22,11 @@ def get_user_contributions(user):
     data_count = []
     data_level = []
     for i in range(len(contributions)):
-        # Se elimina los que no sirven
+        # Delete what's not usefull
         if "data-count" not in contributions[i].attrs:
             continue
         else:
-            # Se agrega los datos al dictionario segun su fecha
+            # Add the data to some list's
             dates.append(contributions[i]["data-date"])
             data_count.append(contributions[i]["data-count"])
             data_level.append(contributions[i]["data-level"])
@@ -40,7 +36,11 @@ def get_user_contributions(user):
 
 # GRAPH GENERATOR
 def star_generator(data):
-    # Se crea una matriz de ceros de tamaño len(data_level)/7 x 7
+    """
+    Function that given a list of contributions generate a graph of it
+    """
+
+    # Create a matrix of zeros
     data_matrix = np.zeros((int(len(data) / 7), 7))
     data_matrix = data_matrix.T
 
@@ -49,7 +49,7 @@ def star_generator(data):
     # data_matrix[2, 5] = 5
     # print(data_matrix)
 
-    # Se llena la matriz con los datos de data_count
+    # Fill the matrix with the data of data_count
     for i in range(len(data)):
         try:
             data_matrix[int(i / 7), i % 7] = data[i]
@@ -57,7 +57,7 @@ def star_generator(data):
         except:  # NO SE LLENA LA MATRIZ COMPLETA, OJO!!!!!
             break
 
-    # Generamos la matriz principal que sera graficada   (matriz y desp pasarlo a lista para graficarlo mejor... en volaa)
+    # Generate the principal matrix which will be graphed
     stars = []
     weeks = 52
     days = 7
@@ -65,8 +65,8 @@ def star_generator(data):
     cols_day = 50
     for week in range(weeks):
         for day in range(days):
-
-            # aca definimos el valor de cuantas estrellas generar en ese dia, y las agregamos a una lista en orden random
+            # Here we define the value of how many starts we need to generate that day
+            # Then we add it to a list in a random order
             level = int(data[day + days * week])
             stars_cuantity = rows_day * cols_day / 6 * (level + 1)
             day_stars = []
@@ -82,28 +82,16 @@ def star_generator(data):
                 row_matrix = []
 
                 for col in range(cols_day):
-                    # aqui vamos agregando un valor binario si hay o no estrella
                     row_matrix.append(day_stars[row * cols_day + col])
 
                 day_matrix.append(row_matrix)
-
             stars.append(day_matrix)
-
     return stars
-
-
-def grafica(star_matrix):
-    star_list = [[], [], [], [], [], [], [], [], [], [], [], [], [], []]
-    cont = 0
-    while True:
-
-        cont += 1
-    print(star_matrix)
 
 
 def generate_plot(dates, data):
     """
-    Funcion que dado un diccionario de contribuciones, genera un grafico con ellas.
+    Function that given a list of contributions generate a graph of it
     """
 
     # Se crea una matriz de ceros de tamaño len(data_level)/7 x 7
